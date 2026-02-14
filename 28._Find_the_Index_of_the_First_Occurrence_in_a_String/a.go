1f03d8e9878dd0b67540916a4449ac8b2a9e6c42
mrezaelange <mrezaelange@outlook.com>
2026-02-14 19:21:30 +0700

    feat: implement strStr function and corresponding tests for finding the index of the first occurrence of a substring

---
+++ 28._Find_the_Index_of_the_First_Occurrence_in_a_String/a.go
@@ -0,0 +1,34 @@
+package a
+
+import "fmt"
+
+// Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+// Example 1:
+
+// Input: haystack = "sadbutsad", needle = "sad"
+// Output: 0
+// Explanation: "sad" occurs at index 0 and 6.
+// The first occurrence is at index 0, so we return 0.
+
+// Example 2:
+
+// Input: haystack = "leetcode", needle = "leeto"
+// Output: -1
+// Explanation: "leeto" did not occur in "leetcode", so we return -1.
+
+func strStr(haystack string, needle string) int {
+	left := 0
+	// curr := ""
+	for right := len(needle); right < len(haystack)+1; right++ {
+		fmt.Println(haystack[left:right])
+		if haystack[left:right] == needle {
+			return left
+		} else {
+			left++
+		}
+		// curr = haystack[left:right]
+	}
+
+	return -1
+}
